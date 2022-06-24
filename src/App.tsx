d6d633bb695d28e0a162f6d186a9d3128ecd2277
Yaroslav Sedyukov <sedyukovwork@gmail.com>
2022-06-24 15:22:29 +0700

    feature: add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 function App(): JSX.Element {
   return (
-    <div className="App" />
+    <div className="App">
+      <div className="text-4xl">s</div>
+    </div>
   );
 }
 
